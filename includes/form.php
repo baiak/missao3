62694869b0849427c13f60d2edde1bf296353eb2
baiak <jonathanbaiak@gmail.com>
2020-12-16 15:50:20 -0300

    inserindo validaçoes
    Aplicado correçoes no HTML

--- includes/form.php
+++ includes/form.php
@@ -1,41 +1,55 @@
 <?php include('includes/selectPaises.php');
  $select = new Select();
 ?>
-<form>
+<form class="needs-validation" novalidate>
     <div class="mb-3">
         <label for="exampleInputEmail1" class="form-label">E-mail</label>
-        <input type="email" class="form-control padding disabled" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="madruguinha@gmail.com" disabled>
+        <input type="email" class="form-control padding" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="madruguinha@gmail.com" id="email" required>
+        <div class="valid-feedback"> Ok</div>
+        <div class="invalid-feedback">Preencha o email!</div>
 
     </div>
     <div class="mb-3">
         <label for="exampleInputPassword1" class="form-label">Sexo</label>
-        <select class="form-select padding disabled" aria-label="sexo" disabled>
+        <select class="form-select padding" aria-label="sexo" required>
             <option selected></option>
             <option value="1">Masculino</option>
             <option value="2">Feminino</option>
-
         </select>
+        <div class="valid-feedback"> Ok</div>
+        <div class="invalid-feedback">Selecione um sexo!</div>
     </div>
 
     Data de nascimento<br>
     <div class="row">
 
         <div class="col">
-            <input type="text" class="form-control padding disabled" placeholder="02" aria-label="Dia" disabled>
+            <input type="text" class="form-control padding " placeholder="02" aria-label="Dia" required>
+            <div class="valid-feedback"> Ok</div>
+            <div class="invalid-feedback">Preencha o dia do nascimento</div>
         </div>
+
         <div class="col">
-            <input type="text" class="form-control padding disabled" placeholder="09" aria-label="mes" disabled>
+            <input type="text" class="form-control padding " placeholder="09" aria-label="mes" required>
+            <div class="valid-feedback"> Ok</div>
+            <div class="invalid-feedback">Preencha o mês de nascimento</div>
         </div>
+
         <div class="col">
-            <input type="text" class="form-control padding disabled" placeholder="1923" aria-label="ano" disabled>
+            <input type="text" class="form-control padding " placeholder="1923" aria-label="ano" required>
+            <div class="valid-feedback"> Ok</div>
+            <div class="invalid-feedback">Preencha o ano de nascimento</div>
         </div>
 
     </div><br>
     <p>País ou região<p>
-    <select class="form-select padding" aria-label="País ou região">
+    <select class="form-select padding" aria-label="País ou região" required>
         <option selected>Mexico</option>
         <?php echo $select->paises(); ?>
     </select>
+    <div class="valid-feedback"> Ok</div>
+    <div class="invalid-feedback">Selecione um país ou região</div>
+
 
     <div class="form-check spacingHor">
         <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
