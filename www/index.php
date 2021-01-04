90a5c81217ce652050f1f3701a19ac7a6d1a967f
baiak <jonathanbaiak@gmail.com>
2021-01-03 20:46:35 -0800

    branch para missao4

---
+++ www/index.php
@@ -0,0 +1,70 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+
+    <!-- CSS customizado-->
+    <link rel="stylesheet" href="CSS/custom.css" />
+
+    <link rel="stylesheet" href="CSS/indexContentStyles.css" />
+
+
+
+    <title>Editar perfil - Spotify</title>
+</head>
+<body>
+<div id="header">
+    <div class="container-sm" style="background-color:black">
+
+        <?php include('includes/header_No_login.php'); ?>
+    </div>
+</div>
+<!--corpo-->
+<div class="container-fluid">
+
+        <!--conteudo principal-->
+
+             <?php include('includes/indexContent.php'); ?>
+
+        <!--conteudo principal fim-->
+
+
+</div><!--fim do container-->
+</div><!--fim do corpo-->
+<div class="rodape">
+    <?php include('includes/footer.php'); ?>
+</div>
+
+
+
+<!--<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js" integrity="sha384-q2kxQ16AaE6UbzuKqyBE9/u/KzioAlnx2maXQHiDX9d4/zp8Ok3f+M7DPm+Ib6IU" crossorigin="anonymous"></script>!-->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.min.js" integrity="sha384-pQQkAEnwaBkjpqZ8RU1fF1AKtTcHJwFl3pblpTlHXybJjHpMYo79HY3hIi4NKxyj" crossorigin="anonymous"></script>
+<script>
+    (function () {
+        'use strict'
+
+        // Fetch all the forms we want to apply custom Bootstrap validation styles to
+        var forms = document.querySelectorAll('.needs-validation')
+
+        // Loop over them and prevent submission
+        Array.prototype.slice.call(forms)
+            .forEach(function (form) {
+                form.addEventListener('submit', function (event) {
+                    if (!form.checkValidity()) {
+                        event.preventDefault()
+                        event.stopPropagation()
+                    }
+
+                    form.classList.add('was-validated')
+                }, false)
+            })
+    })()
+</script>
+
+</body>
+</html>
