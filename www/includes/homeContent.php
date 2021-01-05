047c822557296ce844ef1911bc225e80b3dd7abc
baiak <jonathanbaiak@gmail.com>
2021-01-04 21:09:16 -0800

    ajustes no banner retro
    Ajustando o breakpoint do banner retro

--- www/includes/homeContent.php
+++ www/includes/homeContent.php
@@ -7,45 +7,53 @@
   </div>
 </div>
 
-<div class="container-fluid" >
+<div class="container-fluid ">
    <div class="row banner retro">
-    <div  class="col-md-3">
-      <div class="box-retro">
-        <h3>Retrospectiva 2020</h3>
-      </div>
-    </div>
+       <div class="container">
+           <div class="row justify-content-center">
+               <div  class="col-sm" style="max-width: 500px; text-align:right;">
+                   <div class="box-retro">
+                       <h3>Retrospectiva 2020</h3>
+                   </div>
+               </div>
 
-    <div class="col-md-3" >
-     Retrospectiva 2020
-      <h1>Veja o que rolou no seu Spotify</h1><br>
-      Descubra os artistas, músicas e podcasts que te ajudaram a sobreviver ao ano mais longo da história.<br>
-      <p></p>
-      <p><button class="btn btn-primary retro-btn">Confira sua retrospectiva</button></p>
-    </div>
+               <div class="col-sm"  style="max-width: 450px; text-align: left;">
+                   Retrospectiva 2020
+                   <h1>Veja o que rolou no seu Spotify</h1><br>
+                   Descubra os artistas, músicas e podcasts que te ajudaram a sobreviver ao ano mais longo da história.<br>
+                   <p></p>
+                   <p><button class="btn btn-primary retro-btn">Confira sua retrospectiva</button></p>
+               </div>
+           </div>
+       </div>
+
 
    </div>
 </div>
 
-<div class="row banner lancamentos">
-    <div class="col-10">
-        <h1>É música que você quer?</h1>
-        <br>
-        <h3>Conheça os melhores lançamentos do momento.</h3>
-        <br>
-        <p></p>
-        <p><button class="btn btn-primary lancamentos-btn">Abrir o web player</button></p>
-        <br>
-        <div class="container-md">
-            <div class="row row-cols-3">
-                <div class="col capas"><a href="#"><img class="img-fluid"  src="../img_content/porcaVeia.jpg" width="391" height="397"/></a></div>
-                <div class="col capas"><a href="#"><img class="img-fluid"  src="../img_content/lobisomem.jpeg" width="391" height="397"/></a></div>
-                <div class="col capas"><a href="#"><img class="img-fluid"  src="../img_content/baitaca.jpeg" width="391" height="397"/></a> </div>
-                <div class="col capas"><a href="#"><img  class="img-fluid" src="../img_content/seuMadruga.jpeg" width="391" height="397" /></a> </div>
-                <div class="col capas" ><a href="#"><img class="img-fluid" src="../img_content/ledZeppelin.jpeg" width="391" height="397" /></a> </div>
-                <div class="col capas"><a href="#"><img  class="img-fluid" src="../img_content/creed.jpeg" width="391" height="397"/></a></div>
+<div class="container-fluid">
+    <div class="row banner lancamentos">
+        <div class="col-10">
+            <h1>É música que você quer?</h1>
+            <br>
+            <h3>Conheça os melhores lançamentos do momento.</h3>
+            <br>
+            <p></p>
+            <p><button class="btn btn-primary lancamentos-btn">Abrir o web player</button></p>
+            <br>
+            <div class="container-md">
+                <div class="row row-cols-3">
+                    <div class="col capas"><a href="#"><img class="img-fluid"  src="../img_content/porcaVeia.jpg" width="391" height="397"/></a></div>
+                    <div class="col capas"><a href="#"><img class="img-fluid"  src="../img_content/lobisomem.jpeg" width="391" height="397"/></a></div>
+                    <div class="col capas"><a href="#"><img class="img-fluid"  src="../img_content/baitaca.jpeg" width="391" height="397"/></a> </div>
+                    <div class="col capas"><a href="#"><img  class="img-fluid" src="../img_content/seuMadruga.jpeg" width="391" height="397" /></a> </div>
+                    <div class="col capas" ><a href="#"><img class="img-fluid" src="../img_content/ledZeppelin.jpeg" width="391" height="397" /></a> </div>
+                    <div class="col capas"><a href="#"><img  class="img-fluid" src="../img_content/creed.jpeg" width="391" height="397"/></a></div>
+                </div>
             </div>
         </div>
-    </div>
+</div>
+
 
 
 </div>
